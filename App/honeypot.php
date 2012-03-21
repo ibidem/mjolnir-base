0a060fb98fe7af0cff91fce2ab708cb9d8035493
srcspider <source.spider@gmail.com>
2012-03-21 18:37:46 +0200

    fix'ed honeypot

--- App/honeypot.php
+++ App/honeypot.php
@@ -8,6 +8,8 @@ class Layer_HTTP extends \kohana4\base\Layer_HTTP {}
 class Layer_HTML extends \kohana4\base\Layer_HTML {}
 class Layer_MVC extends \kohana4\base\Layer_MVC {}
 class Route_Regex extends \kohana4\base\Route_Regex {}
+class Route_Path extends \kohana4\base\Route_Path {}
+class Route_Pattern extends \kohana4\base\Route_Pattern {}
 class Params extends \kohana4\base\Params {}
 class Relay extends \kohana4\base\Relay {}
 class Exception extends \kohana4\base\Exception {}
