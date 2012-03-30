512751eeb6ae595acbccba1730e34632abd35f24
srcspider <source.spider@gmail.com>
2012-03-31 00:00:21 +0300

    honeypot generation support

--- App/honeypot.php
+++ App/honeypot.php
@@ -1,23 +1,41 @@
-<?php # IDE honeypot :)
+<?php namespace app;
 
-namespace app;
+// This is a IDE honeypot. :)
 
-class Instantiatable extends \kohana4\base\Instantiatable {}
-class Layer extends \kohana4\base\Layer {}
-class Layer_HTTP extends \kohana4\base\Layer_HTTP {}
-class Layer_HTML extends \kohana4\base\Layer_HTML {}
-class Layer_MVC extends \kohana4\base\Layer_MVC {}
-class Route_Regex extends \kohana4\base\Route_Regex {}
-class Route_Path extends \kohana4\base\Route_Path {}
-class Route_Pattern extends \kohana4\base\Route_Pattern {}
-class Params extends \kohana4\base\Params {}
-class Relay extends \kohana4\base\Relay {}
-class Exception extends \kohana4\base\Exception {}
+// HowTo: minion honeypot -n "kohana4\\base"
+
+class Controller_HTTP extends \kohana4\base\Controller_HTTP {}
+class Controller extends \kohana4\base\Controller {}
+class Event extends \kohana4\base\Event {}
 class Exception_NotApplicable extends \kohana4\base\Exception_NotApplicable {}
 class Exception_NotFound extends \kohana4\base\Exception_NotFound {}
-class Controller extends \kohana4\base\Controller {}
-class View extends \kohana4\base\View {}
-class Event extends \kohana4\base\Event {}
+class Exception extends \kohana4\base\Exception {}
+class Flags extends \kohana4\base\Flags {}
+class Instantiatable extends \kohana4\base\Instantiatable {}
+class Lang extends \kohana4\base\Lang {}
+class Layer_HTML extends \kohana4\base\Layer_HTML {}
+class Layer_HTTP extends \kohana4\base\Layer_HTTP {}
+class Layer_MVC extends \kohana4\base\Layer_MVC {}
+class Layer_PlainText extends \kohana4\base\Layer_PlainText {}
+class Layer_TaskRunner extends \kohana4\base\Layer_TaskRunner {}
+class Layer extends \kohana4\base\Layer {}
+class Params extends \kohana4\base\Params {}
+class Relay extends \kohana4\base\Relay {}
+class Route_Path extends \kohana4\base\Route_Path {}
+class Route_Pattern extends \kohana4\base\Route_Pattern {}
+class Route_Regex extends \kohana4\base\Route_Regex {}
+class SQL extends \kohana4\base\SQL {}
+class SQLDatabase extends \kohana4\base\SQLDatabase {}
+class SQLStatement extends \kohana4\base\SQLStatement {}
+class Task_Find_Class extends \kohana4\base\Task_Find_Class {}
+class Task_Find_File extends \kohana4\base\Task_Find_File {}
+class Task_Honeypot extends \kohana4\base\Task_Honeypot {}
+class Task_Make_Config extends \kohana4\base\Task_Make_Config {}
+class Task_Migrate extends \kohana4\base\Task_Migrate {}
+class Task_Versions extends \kohana4\base\Task_Versions {}
+class Task extends \kohana4\base\Task {}
 trait Trait_Document { use \kohana4\base\Trait_Document; }
 trait Trait_FileBased { use \kohana4\base\Trait_FileBased; }
 trait Trait_Meta { use \kohana4\base\Trait_Meta; }
+class View extends \kohana4\base\View {}
+class Writer_Console extends \kohana4\base\Writer_Console {}
---
+++ Task/Honeypot.php
@@ -0,0 +1,88 @@
+<?php namespace kohana4\base;
+
+/**
+ * @package    Kohana4
+ * @category   Base
+ * @author     Kohana Team
+ * @copyright  (c) 2008-2012 Kohana Team
+ * @license    http://kohanaframework.org/license
+ */
+class Task_Honeypot extends \app\Task
+{
+	/**
+	 * Execute task.
+	 */
+	public function execute()
+	{
+		$ns = $this->config['namespace'];
+		$modules = \array_flip(\app\CFS::get_modules());
+		if (isset($modules[$ns]))
+		{
+			$path = $modules[$ns];
+			$files = static::files($path);
+			$output = '<?php namespace app;';
+			$output .= PHP_EOL.PHP_EOL.'// This is a IDE honeypot. :)';
+			$output .= PHP_EOL.PHP_EOL.'// HowTo: minion honeypot -n "'.
+					\str_replace('\\', '\\\\', $this->config['namespace']).'"'.
+					PHP_EOL.PHP_EOL;
+			
+			foreach ($files as $file)
+			{
+				if (\preg_match('#^Trait_#', $file))
+				{
+					$output .= 'trait '.$file.' { use \\'.$ns.'\\'.$file.'; }'.PHP_EOL;
+				}
+				else # is not a trait
+				{
+					$output .= 'class '.$file.' extends \\'.$ns.'\\'.$file.' {}'.PHP_EOL;
+				}
+			}
+			$dir = $path.DIRECTORY_SEPARATOR.\app\CFS::APPDIR.DIRECTORY_SEPARATOR;
+			if ( ! \is_dir($dir))
+			{
+				\mkdir($dir, 0777, true);
+			}
+			\file_put_contents($dir.'honeypot'.EXT, $output);
+			$this->writer
+				->status
+					(
+						'Info', 
+						'Succesfully created ['.\str_replace(DOCROOT, '', $dir.'honeypot'.EXT).']'
+					)
+				->eol();
+		}
+		else # namespace does not exist
+		{
+			throw \app\Exception_NotFound::instance
+				('No such namespace registered; please check your [environment.php] file.');
+		}
+	}
+	
+	protected static function files($path, $prefix = '')
+	{
+		static $exclude_pattern = array('..', '.', \app\CFS::APPDIR);
+		
+		$ext_pattern = '#'.\str_replace('.', '\.', EXT).'$#';
+		$files = \array_diff(\scandir($path), $exclude_pattern);
+		$clean_files = array();
+		foreach ($files as $file)
+		{
+			if (\is_dir($path.DIRECTORY_SEPARATOR.$file))
+			{
+				$clean_files = \array_merge
+					(
+						$clean_files, 
+						static::files($path.DIRECTORY_SEPARATOR.$file, $prefix.$file.'_')
+					);
+			}
+			else if (\preg_match($ext_pattern, $file))
+			{
+				// found a class file
+				$clean_files[] = $prefix.\preg_replace($ext_pattern, '', $file);
+			}
+		}
+		
+		return $clean_files;
+	}
+	
+} # class
