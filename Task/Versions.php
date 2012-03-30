7f157d6bc06e4c486e709fb581f12b1b67b50ecf
srcspider <source.spider@gmail.com>
2012-03-31 00:01:53 +0300

    version task support

---
+++ Task/Versions.php
@@ -0,0 +1,50 @@
+<?php namespace kohana4\base;
+
+/**
+ * @package    Kohana4
+ * @category   Base
+ * @author     Kohana Team
+ * @copyright  (c) 2008-2012 Kohana Team
+ * @license    http://kohanaframework.org/license
+ */
+class Task_Versions extends \app\Task
+{
+	/**
+	 * @var array
+	 */
+	protected static $defaults = array
+		(
+			'name' => null, 
+			'major' => '0',
+			'minor' => '0',
+			'hotfix' => '0',
+			'tag' => null,
+		);
+	
+	/**
+	 * Execute task.
+	 */
+	public function execute()
+	{
+		$versions = \app\CFS::config('version');
+		$length = 10;
+		foreach (\array_keys($versions) as $key)
+		{
+			if ($length < \strlen($key))
+			{
+				$length = \strlen($key);
+			}
+		}
+		$format = ' %'.$length.'s - %s';
+		foreach ($versions as $key => $info)
+		{
+			$v = \app\CFS::merge(static::$defaults, $info);
+			$version = $v['major'].'.'.$v['minor']
+				. ($v['hotfix'] !== '0' ? '.'.$v['hotfix'] : '')
+				. ($v['tag'] !== null ? '-'.$v['tag'] : '');
+
+			$this->writer->writef($format, $key, $version)->eol();
+		}
+	}
+	
+} # class
