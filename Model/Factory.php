d32a0b0cb713764c9e80dae3b0b73ec94335cf06
srcspider <source.spider@gmail.com>
2012-04-05 22:09:18 +0300

    model factory

---
+++ Model/Factory.php
@@ -0,0 +1,59 @@
+<?php namespace ibidem\base;
+
+/** 
+ * @package    ibidem
+ * @category   Base
+ * @author     Ibidem Team
+ * @copyright  (c) 2008-2012 Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+abstract class Model_Factory
+{
+	/**
+	 * Return validator for fields. Can be used in cases where all errors need
+	 * to be reported for a form even though we already know there are some
+	 * errors that would prevent assembly.
+	 * 
+	 * @param array fields
+	 * @return Validation
+	 */
+	public static function validator(array $fields)
+	{
+		throw \app\Exception_NotApplicable::instance
+			('The [validator] method not implmented in ['.\called_class().'].');
+	}
+	
+	/**
+	 * Assemble fields after validation.
+	 * 
+	 * @param array fields
+	 */
+	public static function assemble(array $fields)
+	{
+		throw \app\Exception_NotApplicable::instance
+			('The [assemble] method not implmented in ['.\called_class().'].');
+	}
+	
+	/**
+	 * Fabricate the model.
+	 * 
+	 * @param array fields required for creation
+	 * @return Validation Validator on error, NULL on success
+	 */
+	final public static function factory(array $fields)
+	{
+		$validator = static::validator($fields);
+		
+		if ($validator === null || $validator->check())
+		{
+			static::assemble($fields);
+		}
+		else # did not pass validation
+		{
+			return $validator;
+		}
+		
+		return null;
+	}
+	
+} # class
