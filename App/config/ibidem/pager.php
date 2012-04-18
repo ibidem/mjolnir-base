a51246a78db12fe2d76eb44aab0200dbb9063254
srcspider <source.spider@gmail.com>
2012-04-18 14:21:15 +0300

    working Pager

---
+++ App/config/ibidem/pager.php
@@ -0,0 +1,4 @@
+<?php return array
+	(
+		'view.default' => 'ibidem/pager',
+	);
--- Pager.php
+++ Pager.php
@@ -8,40 +8,235 @@
  * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
  */
 class Pager extends \app\Instantiatable
-	implements \ibidem\types\Renderable
-{
+	implements 
+		\ibidem\types\Renderable, 
+		\ibidem\types\FileBased,
+		\ibidem\types\Pager
+{	
 	/**
-	 * @var string 
+	 * @var array 
 	 */
-	private $base_url;
+	private $options = array
+		(
+			'currentpage' => null,  # selected page
+			'has_ruler'   => null,  # show ruler on pager
+			'order'       => \ibidem\types\Pager::ascending,
+		
+			'entry_name' => array
+				(
+					'plural' => 'Entries',
+					'singular' => 'Entry'
+				),
+		
+			'bookmark' => array
+				(
+					'anchor' => null,
+					'page'   => 0,
+					'entry'  => 0,
+				),
+		
+			// computed
+			'pagecount' => 0,
+			'startpoint' => 0,
+			'endpoint' => 0,
+			'startellipsis' => 0,
+			'endellipsis' => 0,
+		);
 	
 	/**
-	 * @param string base_url
-	 * @return \ibidem\base\Pager 
+	 * @param integer totalitems
+	 * @param integer url_base
+	 * @param integer pagerdiff
+	 * @param integer pagelimit 
+	 * @return \ibidem\base\Pager instance
 	 */
-	public function base_url($base_url)
+	public static function instance($totalitems = 0, $url_base = '', $pagediff = 4, $pagelimit = 20)
 	{
-		$this->base_url = $base_url;
+		$instance = parent::instance();
+		$instance->options['totalitems'] = $totalitems;
+		$instance->options['url_base'] = $url_base;
+		$instance->options['pagediff'] = $pagediff;
+		$instance->options['pagelimit'] = $pagelimit;
+		
+		return $instance;
+	}
+		
+	/**
+	 * @param integer page
+	 * @return \ibidem\base\Pager $this 
+	 */
+	public function currentpage($page)
+	{
+		$this->options['currentpage'] = $page;
 		return $this;
 	}
 	
 	/**
-	 * @param string absolute path
-	 * @return \ibidem\base\Pager 
+	 * @param integer page
+	 * @return \ibidem\base\Pager $this 
 	 */
-	public function view($absolute_path)
+	public function bookmark($page)
 	{
-		$this->view = $absolute_path;
+		$this->options['bookmark']['entry'] = $page;
 		return $this;
+	}
+	
+	/**
+	 * Generic anchor; for example, in the case of html this will result in
+	 * a '#'.$anchor in the url; other systems might behave differently.
+	 * 
+	 * @param string anchor
+	 * @return \ibidem\types\Pager $this
+	 */
+	function bookmark_anchor($anchor)
+	{
+		$this->options['bookmark']['anchor'] = $anchor;
+		return $this;
+	}
+	
+	/**
+	 * @param string plural version
+	 * @param string singular version; assumed plural if not specified
+	 * @return \ibidem\base\Pager $this
+	 */
+	public function entry_name($plural, $singular = null)
+	{
+		$singular or $singular = $plural;
+		$this->options['entry_name']['plural'] = $plural;
+		$this->options['entry_name']['singular'] = $singular;
+		
+		return $this;
+	}
+
+	/**
+	 * @param string order
+	 * @return \ibidem\types\Pager $this
+	 */
+	public function order($order)
+	{
+		$this->options['order'] = $order;
+		return $this;
+	}
+	
+	/**
+	 * @param integer total items
+	 * @return \ibidem\base\Pager $this
+	 */
+	public function totalitems($totalitems)
+	{
+		$this->options['totalitems'] = $totalitems;
+		return $this;
+	}		
+	
+	/**
+	 * @param string base_url
+	 * @return \ibidem\base\Pager $this
+	 */
+	public function url_base($url_base)
+	{
+		$this->options['url_base'] = $url_base;
+		return $this;
+	}
+
+	/**
+	 * @param integer pagediff
+	 * @return \ibidem\base\Pager $this 
+	 */
+	public function pagediff($pagediff)
+	{
+		$this->options['pagediff'] = $pagediff;
+		return $this;
+	}
+	
+	/**
+	 * @param integer pagelimit
+	 * @return \ibidem\base\Pager $this
+	 */
+	public function pagelimit($pagelimit)
+	{
+		$this->options['pagelimit'] = $pagelimit;
+		return $this;
+	}
+	
+	/**
+	 * Setup pager 
+	 */
+	private function setup()
+	{
+		// is file set?
+		if ( ! $this->get_file())
+		{
+			$pager_config = \app\CFS::config('ibidem/pager');
+			$this->file($pager_config['view.default']);
+		}
+		
+		// extract options to work in context
+		\extract($this->options, EXTR_REFS);
+		
+		// is the ruler showed?
+		$has_ruler !== null or $has_ruler = (empty($currentpage) ? false : true);
+	
+		// calculate page count
+		$pagecount = \ceil($totalitems / $pagelimit);
+		
+		// do we have a bookmark?
+		if ($bookmark['entry'] != 0)
+		{
+			$bookmark['page'] = \ceil($bookmark['entry'] / $pagelimit);
+		}
+		
+		$startpoint = ($pagecount < $pagediff ? $pagecount : $pagediff);
+		$endpoint = ($pagecount - $pagediff + 1 < 1 ? 1 : $pagecount - $pagediff + 1);
+		
+		$startellipsis = $startpoint;
+		$endellipsis = $endpoint;
+		
+		if ($endellipsis == $bookmark['page'])
+		{
+			$endellipsis -= 1;
+		}
+		
+		if ($startpoint >= $currentpage - $pagediff)
+		{
+			$startpoint = $pagediff * 3 + 1 - 3;
+			$startellipsis = 0;
+		}
+		
+		if ($endpoint <= $currentpage + $pagediff)
+		{
+			$endpoint = $pagecount - ($pagediff * 3 - 3);
+			$endellipsis = 0;
+		}
 	}
 	
 	/**
 	 * @return string
 	 */
 	public function render()
-	{
-		// @todo render \ibidem\base\Pager
-		return '[todo pager]';
+	{		
+		// setup pager
+		$this->setup();
+		
+		// extract view paramters into current scope as references to paramter
+		// array in the view itself, skipping over already defined variables
+		\extract($this->options, EXTR_REFS);
+		
+		// start capture
+		\ob_start();
+		try
+		{
+			// include in current scope
+			include $this->file;
+		}
+		catch (\Exception $error)
+		{
+			// cleanup
+			\ob_end_clean();
+			// re-throw
+			throw $error;
+		}
+		// success
+		return \ob_get_clean();
 	}
 	
 	/**
@@ -60,5 +255,59 @@ class Pager extends \app\Instantiatable
 					('Casting to string not allowed for Pagers.')
 			);
 	}
+	
+# FileBased trait
+	
+	/**
+	 * @var string view file
+	 */
+	protected $file;
+		
+	/**
+	 * @param string file 
+	 * @return \ibidem\base\View $this
+	 */
+	public function file($file)
+	{
+		$file_path = \app\CFS::file($file);
+		// found file?
+		if ($file_path === null)
+		{
+			throw new \app\Exception_NotFound
+				("Required file [$file] not found.");
+		}
+		else # found file
+		{
+			$this->file = $file_path;
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * @param string explicit file path
+	 * @return \ibidem\base\View $this
+	 */
+	public function file_path($file)
+	{
+		$this->file = \realpath($file);
+		if ($file !== null && ! \file_exists($this->file))
+		{
+			throw new \app\Exception_NotFound
+				("Required file [$file] not found.");
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * @return string file path
+	 */
+	public function get_file()
+	{
+		return $this->file;
+	}
+	
+# /FileBased trait
 
 } # class
