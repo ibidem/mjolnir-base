f441bc8b50af70dcdf3ed013ff68e81330bb3869
srcspider <source.spider@gmail.com>
2012-04-20 16:06:56 +0300

    exception for security related errors

---
+++ Exception/NotAllowed.php
@@ -0,0 +1,25 @@
+<?php namespace ibidem\base;
+
+/**
+ * @package    ibidem
+ * @category   Base
+ * @author     Ibidem
+ * @copyright  (c) 2012, Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+class Exception_NotAllowed extends \app\Exception_NotApplicable
+{
+	/**
+	 * @var string
+	 */
+	protected $type = \ibidem\types\Exception::NotAllowed;
+	
+	/**
+	 * @return string title
+	 */
+	public function title()
+	{
+		return empty($this->title) ? 'Not Allowed' : $this->title;
+	}
+
+} # class
