45760cf26d8326c1076d4264b31de3bc4e05db75
srcspider <source.spider@gmail.com>
2012-04-20 16:02:33 +0300

    finished validator

--- Validator.php
+++ Validator.php
@@ -15,11 +15,28 @@ class Validator extends \app\Instantiatable
 	private $fields;
 	
 	/**
-	 * @var 
+	 * @var array
 	 */
-	public static function instance(array $fields)
+	private $errors;
+	
+	/**
+	 * @var array 
+	 */
+	private $rules;
+	
+	/**
+	 * @param string config with errors (should contain "errors" on route)
+	 * @param array fields
+	 * @return \ibidem\base\Validator 
+	 */
+	public static function instance(array $errors = null, array $fields = null)
 	{
-		$this->fields($fields);
+		$instance = parent::instance();
+		$instance->rules = array();
+		$errors and $instance->errors($errors);
+		$fields and $instance->fields($fields);
+		
+		return $instance;
 	}
 	
 	/**
@@ -33,18 +50,81 @@ class Validator extends \app\Instantiatable
 	}
 	
 	/**
-	 * @param array fields
+	 * @param array errors
 	 * @return \ibidem\base\Validator $this
 	 */
-	public function rule(array $args)
+	public function errors(array $errors)
+	{
+		
+		$this->errors = $errors;
+		return $this;
+	}
+	
+	/**
+	 * @param array $args
+	 * @return \ibidem\base\Validator $this
+	 */
+	public function rule($args)
 	{
 		$args = \func_get_args();
 		
-		$field = \array_shift($args);
-		$callback = \array_shift($args);
-		\array_unshift($args, $this->fields[$field]);
-		
+		$this->rules[] = $args;
+
 		return $this;
+	}
+	
+	/**
+	 * @return array|null array of errors on failure, null on success
+	 */
+	public function validate()
+	{
+		$errors = array();
+		foreach ($this->rules as $args)
+		{
+			$field = \array_shift($args);
+			$callback = \array_shift($args);
+			\array_unshift($args, $this->fields[$field]);
+			
+			if (\strpos($callback, '::') === false)
+			{
+				// default to generic rule set
+				$callfunc = '\app\ValidatorRules::'.$callback;
+			}
+			else
+			{
+				$callfunc = $callback;
+			}
+			
+
+			if ( ! \call_user_func_array($callfunc, $args))
+			{
+				// gurantee error field exists as an array
+				isset($errors[$field]) or $errors[$field] = array();
+				
+				if ( ! isset($this->errors[$field]))
+				{
+					throw new \app\Exception_NotFound
+						("Missing error messages for [$field].");
+				}
+				
+				if ( ! isset($this->errors[$field][$callback]))
+				{
+				
+					// generic rules won't work since everything will just look
+					// wrong if we print the same message two or three times as
+					// a consequence of the user getting several things wrong 
+					// for the same field
+					throw new \app\Exception_NotFound
+						("Missing error message for when [$field] fails [$callback].");
+				}
+				
+				// add errors based on error field
+				$errors[$field][$callback] = $this->errors[$field][$callback];
+			}
+		}
+		
+		// return null if no errors or array with error messages
+		return empty($errors) ? null : $errors;
 	}
 
 } # class
---
+++ ValidatorRules.php
@@ -0,0 +1,44 @@
+<?php namespace ibidem\base;
+
+/**
+ * This class should only ever be extended at the project level. For all other
+ * cases use callbacks.
+ * 
+ * @package    ibidem
+ * @category   Library
+ * @author     Ibidem
+ * @copyright  (c) 2012, Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+class ValidatorRules
+{
+	/**
+	 * @param mixed field
+	 * @return boolean 
+	 */
+	public static function not_empty($field)
+	{
+		return ! empty($field);
+	}
+	
+	/**
+	 * @param string field
+	 * @param integer maxlength
+	 * @return boolean 
+	 */
+	public static function max_length($field, $maxlength)
+	{
+		return \strlen($field) <= $maxlength;
+	}
+	
+	/**
+	 * @param string field
+	 * @param integer minlength
+	 * @return boolean 
+	 */
+	public static function min_length($field, $minlength)
+	{
+		return \strlen($field) >= $minlength;
+	}
+
+} # class
