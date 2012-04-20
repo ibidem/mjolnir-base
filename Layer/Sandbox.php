6f35a8ae54950f54c22c7a01d3908e94afd60d9d
srcspider <source.spider@gmail.com>
2012-04-20 16:05:34 +0300

    sandbox layer, for executing simple functions \w more complex layers on top (access, http, etc)

---
+++ Layer/Sandbox.php
@@ -0,0 +1,58 @@
+<?php namespace ibidem\base;
+
+/**
+ * @package    ibidem
+ * @category   Base
+ * @author     Ibidem
+ * @copyright  (c) 2012, Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+class Layer_Sandbox extends \app\Layer
+{
+	/**
+	 * @var callback 
+	 */
+	private $callback;
+	
+	/**
+	 * @var array
+	 */
+	private $relay;
+	
+	/**
+	 * @param array relay
+	 * @return \ibidem\access\Layer_NULL $this
+	 */
+	public function relay($relay)
+	{
+		$this->relay = $relay;
+		return $this;
+	}
+	
+	/**
+	 * Execute the layer.
+	 */
+	public function execute()
+	{
+		try
+		{
+			$callback = $this->callback;
+			$this->contents($callback($this->relay));
+		}
+		catch (\Exception $exception)
+		{
+			$this->exception($exception);
+		}
+	}
+	
+	/**
+	 * @param callback callback
+	 * @return \ibidem\base\Layer_NULL $this
+	 */
+	public function caller($callback)
+	{
+		$this->callback = $callback;
+		return $this;
+	}
+
+} # class
