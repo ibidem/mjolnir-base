2e10e26ed75fba7afc175d83d8893d004220f41d
srcspider <source.spider@gmail.com>
2012-04-26 23:33:31 +0300

    switching script loaders to yepnope since it can handle google

--- Layer/HTML.php
+++ Layer/HTML.php
@@ -188,6 +188,12 @@ class Layer_HTML extends \app\Layer
 			$html_before .= '<meta name="msapplication-starturl" content="'.$this->meta['application_starturl'].'">';
 		}
 		
+		if ( ! empty($this->meta['scripts']))
+		{
+			// javascript loader
+			$html_before .= '<script type="text/javascript" src="//'.$ibidem_base['domain'].$ibidem_base['path'].'media/static/yepnope.latest-min.js"></script>';
+		}
+		
 		// close head section
 		$html_before .= '</head><body>';
 		// css switch for more streamline style transitions
@@ -210,17 +216,14 @@ class Layer_HTML extends \app\Layer
 		$body = "\n\n";
 		if ( ! empty($this->meta['scripts']))
 		{
-			$body .= '<script>'
-				. '/* NBL.js (c) 2010 Berklee. Licensed under the MIT license. */'."\n"
-				. 'this.nbl={c:document,q:{},n:null,l:function(a){var b,c,x,y,z,s,l,i=j=0,m=this;m.h=m.c.head||m.c.bodym.c.documentElement||m.h,m.i||(m.s=m.f=0,m.i=setInterval(function(){if(m.o<0||m.s==0)m.i=clearInterval(m.i),m.s>0&&m.f&&m.f(m.q);m.o--},m.o=50));if(a&&a.shift)while(i<a.length)b=a[i],c=a[i+1],x="function",y=typeof b,z=typeof c,l=z==x?c:y==x?b:m.n,y=="number"&&(m.o=b/50),y=="string"&&m.a([b],l),b.shift&&m.a([b.shift(),b],l),!m.f&&l&&(m.f=l),i++},a:function(a,b){var c,d=this,e=a[0].replace(/.+\/|\.min\.js|\.js|\?.+|\W/gi,"");c=d.q[e]=d.c.createElement("script"),c.setAttribute("src",a[0]),c.onload=c.onreadystatechange=function(){var c=this,f=function(){var c=d,f=a[1];c.q[e]=!0,f&&c.l(f),b&&b(),c.s--};if(!c.readyState||/de|te/.test(c.readyState))c.onload=c.onreadystatechange=d.n,f()},d.s++,d.h.appendChild(c)}},'."\n"
-				. 'nbl.l([';
+			$body .= '<script type="text/javascript">yepnope({ load: [';
 			$scripts = $this->meta['scripts'];
 			$body .= '\''.\addslashes(\array_shift($scripts)).'\'';
 			foreach ($scripts as $script)
 			{
 				$body .= ', \''.\addslashes($script).'\'';
 			}
-			$body .= '])</script>';
+			$body .= '] });</script>';
 		}
 		$body .= "</body></html>\n";
 		
