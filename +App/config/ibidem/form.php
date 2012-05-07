7e453c57a69157142f90358cabcb7334abba97f4
srcspider <source.spider@gmail.com>
2012-05-07 10:08:39 +0300

    removed <dl>

--- +App/config/ibidem/form.php
+++ +App/config/ibidem/form.php
@@ -3,7 +3,7 @@
 		'secure.default' => true, # all forms are fully secured; by default
 		'method.default' => \ibidem\types\HTTP::POST,
 		'template.field' => '<dt>:name</dt><dd>:field</dd>',
-		'template.group' => '<fieldset><legend>:legend</legend><dl>:fields</dl></fieldset>',
+		'template.group' => '<fieldset><legend>:legend</legend>:fields</fieldset>',
 	
 		'textarea.rows.default' => 3,
 		'textarea.cols.default' => 80,
