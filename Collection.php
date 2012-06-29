c3f74c37765ab753277753de80568554b1d1f846
srcspider <source.spider@gmail.com>
2012-06-29 18:31:01 +0300

    extra functions

--- Collection.php
+++ Collection.php
@@ -64,4 +64,50 @@ class Collection
 		return $new_array;
 	}
 
+	/**
+	 * Given an array of arrays the function gathers all the values from a given
+	 * key from the inner arrays. If the key is not set in an array the function 
+	 * will ignore that entry and continue processing.
+	 * 
+	 * @param array array of arrays
+	 * @param mixed key
+	 * @return array all values for that key
+	 */
+	static function gather(array $array, $key)
+	{
+		$values = [];
+		foreach ($array as $sub_array)
+		{
+			if (isset($sub_array[$key]))
+			{
+				$values[] = $sub_array[$key];
+			}
+		}
+		
+		return $values;
+	}
+	
+	/**
+	 * Filter array based on filter function; the function should return true 
+	 * for acceptable entries and false for unacceptable entries. Keys will be
+	 * maintained in the filtered version;
+	 * 
+	 * @param array collection
+	 * @param callback filter (key, value)
+	 */
+	static function filter($collection, $filter)
+	{
+		$filtered_collection = [];
+		
+		foreach ($collection as $key => $value)
+		{
+			if ($filter($key, $value))
+			{
+				$filtered_collection[$key] = $value;
+			}
+		}
+		
+		return $filtered_collection;
+	}
+	
 } # class
