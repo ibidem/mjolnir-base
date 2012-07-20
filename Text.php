12354f3c9d3b3d29f1d8e6505ab9b63324feb6ec
srcspider <source.spider@gmail.com>
2012-07-20 17:11:53 +0300

    text class

---
+++ Text.php
@@ -0,0 +1,24 @@
+<?php namespace ibidem\base;
+
+/**
+ * @package    ibidem
+ * @category   Base
+ * @author     Ibidem
+ * @copyright  (c) 2012, Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+class Text extends \app\Instantiatable
+{
+	static function summary($source, $maxlength, $cutoff_text = '...')
+	{
+		if (\strlen($source) <= $maxlength)
+		{
+			return $source;
+		}
+		else # source > $maxlength
+		{
+			return \substr($source, 0, $maxlength - \strlen($cutoff_text)).$cutoff_text;
+		}
+	}
+
+} # class
