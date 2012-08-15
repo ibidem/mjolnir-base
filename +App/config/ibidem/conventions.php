5f141a090cc88657f62649e872e07a1ff85314f7
srcspider <source.spider@gmail.com>
2012-08-15 17:58:12 +0300

    removed obsolete conventions

--- +App/config/ibidem/conventions.php
+++ +App/config/ibidem/conventions.php
@@ -2,9 +2,7 @@
 	(
 		'base_classes' => array
 			(
-				'#^Model_.*$#' => '\app\Model_SQL_Factory',
 				'#^Controller_.*$#' => '\app\Controller_HTTP',
-				'#^Migration_.*$#' => '\app\Migration_Template_MySQL',
 				'#^Task_.*$#' => '\app\Task',
 			),
 	);
