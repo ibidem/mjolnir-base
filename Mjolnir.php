1082301f67a519326eb2a51f131cdeeacf4de243
srcspider <source.spider@gmail.com>
2012-08-19 23:08:31 +0300

    shorthand class for centralizing general public directory tasks

---
+++ Mjolnir.php
@@ -0,0 +1,73 @@
+<?php namespace ibidem\base;
+
+/**
+ * The purpose of this class is to store some of the grunt work that would 
+ * otherwise go into public files, etc.
+ * 
+ * @package    ibidem
+ * @category   Base
+ * @author     Ibidem
+ * @copyright  (c) 2012, Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+class Mjolnir
+{
+	/**
+	 * Shorthand.
+	 * 
+	 * Runs standard http procedures.
+	 */
+	static function www($system_config)
+	{
+		// downtime?
+		if ($system_config['maintanence']['enabled'] && ( ! isset($_GET['passcode']) || $_GET['passcode'] !== $system_config['maintanence']['passcode']))
+		{
+			require 'downtime.php';
+			exit;
+		}
+
+		// set language
+		\app\Lang::lang($system_config['lang']);
+		
+		// check all routes
+		\app\Route::check_all();
+		
+		// go though all relays
+		\app\Relay::check_all();
+
+		// we failed relays
+		\header("HTTP/1.0 404 Not Found");
+		echo '404 - Not Found';
+		exit(1);
+	}
+
+	/**
+	 * Shorthand.
+	 * 
+	 * Runs standard theme procedures.
+	 */
+	static function themes($system_config)
+	{
+		$stack = function ($relay, $target)
+			{
+				\app\Layer::stack
+					(
+						\app\Layer_HTTP::instance(),
+						\app\Layer_Theme::instance()
+							->relay_config($relay)
+					);
+			};
+
+		\app\Relay::process('\ibidem\theme\Layer_Theme::style', $stack);
+		\app\Relay::process('\ibidem\theme\Layer_Theme::script-map', $stack);
+		\app\Relay::process('\ibidem\theme\Layer_Theme::script-src', $stack);
+		\app\Relay::process('\ibidem\theme\Layer_Theme::script', $stack);
+		\app\Relay::process('\ibidem\theme\Layer_Theme::resource', $stack);
+
+		// we failed relays
+		\header("HTTP/1.0 404 Not Found");
+		echo '404 - Media Not Found';
+		exit(1);
+	}
+	
+} # class
