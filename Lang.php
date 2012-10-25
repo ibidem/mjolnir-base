a5e4f8c047809cf19abdc7ac9887a63f2272db7e
srcspider <source.spider@gmail.com>
2012-10-25 21:15:16 +0300

    language exception changed from not found to regular

--- Lang.php
+++ Lang.php
@@ -14,20 +14,20 @@ class Lang
 	 * @var string
 	 */
 	private static $current_lang = 'en-us';
-	
+
 	/**
 	 * Translate a given term. The translation may not necesarily be from one
 	 * language to another. For example, grammer use:
-	 * 
+	 *
 	 *     Lang::tr(':num people visited London.', ... );
 	 *     // 1 => 1 person visited London.
 	 *     // 2 => 2 people visited London.
 	 *     // 0 => Nobody visited London.
-	 * 
+	 *
 	 * If a term is not defined, it will be returned as-is.
-	 * 
+	 *
 	 * [!!] Keys and terms are seperate entities.
-	 * 
+	 *
 	 * @param string term
 	 * @param array addins
 	 * @param string source language
@@ -39,7 +39,7 @@ class Lang
 		// lang/en-us/ro-ro => translate to en-us from ro-ro
 		// lang/ro-ro/messages
 		// lang/ro-ro/en-us
-		
+
 		$config = \app\CFS::config('lang/'.self::$current_lang.'/'.$from_lang);
 		if ( ! isset($config[$term]))
 		{
@@ -48,11 +48,11 @@ class Lang
 			{
 				\app\Log::message
 					(
-						'ERROR', 
-						"The term [$term] is missing a translation ($from_lang => {$target_lang}).", 
+						'ERROR',
+						"The term [$term] is missing a translation ($from_lang => {$target_lang}).",
 						'lang/'.$from_lang.'/'.self::$current_lang.'/'
 					);
-				
+
 				return \strtr($term, $addins);
 			}
 			else if ($from_lang === self::$current_lang)
@@ -63,11 +63,11 @@ class Lang
 			{
 				\app\Log::message
 					(
-						'ERROR', 
-						"The term [$term] is missing a translation ($from_lang => {$target_lang}).", 
+						'ERROR',
+						"The term [$term] is missing a translation ($from_lang => {$target_lang}).",
 						'lang/'.$from_lang.'/'.self::$current_lang.'/'
 					);
-				
+
 				return $term;
 			}
 		}
@@ -82,24 +82,24 @@ class Lang
 			return $config[$term];
 		}
 	}
-	
+
 	/**
 	 * Get's a language file from the current language's directory.
-	 * 
+	 *
 	 * @return array
 	 */
 	static function file($file)
 	{
 		return \app\CFS::config('lang/'.self::$current_lang.'/'.$file);
 	}
-	
+
 	/**
-	 * Access a specific message identified by the key. 
-	 * 
+	 * Access a specific message identified by the key.
+	 *
 	 * The key MUST be defined.
-	 * 
+	 *
 	 * [!!] Keys and terms are seperate entities.
-	 * 
+	 *
 	 * @param string key
 	 * @param string source language
 	 * @return string
@@ -107,12 +107,12 @@ class Lang
 	static function msg($key, array $addins = null)
 	{
 		$config = \app\CFS::config('lang/'.self::$current_lang.'/messages');
-		
+
 		if ( ! isset($config[$key]))
 		{
-			throw new \app\Exception_NotFound('Missing message ['.$key.'] for language ['.self::$current_lang.'].');
+			throw new \app\Exception('Missing message ['.$key.'] for language ['.self::$current_lang.'].');
 		}
-		
+
 		if ($addins !== null)
 		{
 			// if we have addins, the term matches to a lambda
@@ -124,7 +124,7 @@ class Lang
 			return $config[$key];
 		}
 	}
-			
+
 	/**
 	 * @param string target lang
 	 */
@@ -132,7 +132,7 @@ class Lang
 	{
 		self::$current_lang = $lang;
 	}
-	
+
 	/**
 	 * @return string current target language
 	 */
@@ -140,5 +140,5 @@ class Lang
 	{
 		return self::$current_lang;
 	}
-	
+
 } # class
