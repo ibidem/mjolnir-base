0d57e44a625d8930610674f695b0c859f4d04141
srcspider <source.spider@gmail.com>
2012-11-09 10:32:28 +0200

    minor tweaks

--- Lang.php
+++ Lang.php
@@ -37,10 +37,9 @@ class Lang
 	{
 		// lang/en-us/messages => translate to en-us using messages
 		// lang/en-us/ro-ro => translate to en-us from ro-ro
-		// lang/ro-ro/messages
-		// lang/ro-ro/en-us
 
 		$config = \app\CFS::config('lang/'.self::$current_lang.'/'.$from_lang);
+		
 		if ( ! isset($config[$term]))
 		{
 			$target_lang = self::$current_lang;
