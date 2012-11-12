9735e5052227d800658984574163ab47bc8df977
srcspider <source.spider@gmail.com>
2012-11-12 22:53:10 +0200

    default to read-only permission for files

--- +App/config/mjolnir/base.php
+++ +App/config/mjolnir/base.php
@@ -6,9 +6,9 @@
 		'timezone' => 'Europe/Bucharest',
 		'charset' => 'UTF8',
 		'locale.lang' => 'en_US',
-	
+
 		'development' => false,
 
-		'default.file.permissions' => 655,
+		'default.file.permissions' => 644,
 		'default.dir.permissions' => 755,
 	);
