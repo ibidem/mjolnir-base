9756cd58cde155d1e520b8e5d19a4693d6e1751c
srcspider <source.spider@gmail.com>
2012-11-16 12:42:28 +0200

    baseindent method for indentation normalization

--- Text.php
+++ Text.php
@@ -20,5 +20,54 @@ class Text extends \app\Instantiatable
 			return \substr($source, 0, $maxlength - \strlen($cutoff_text)).$cutoff_text;
 		}
 	}
+	
+	static function baseindent($source, $indent = '', $uniform_tabs = 4, $ignore_zero_indent = true)
+	{
+		// unify tabs
+		if ($uniform_tabs !== null)
+		{
+			$source = \str_replace("\t", \str_repeat(' ', $uniform_tabs), $source);
+		}
+		
+		// split into lines
+		$lines = \explode("\n", $source);
+		
+		// detect indent level
+		$min_length = null;
+		foreach ($lines as $line)
+		{
+			if (\preg_match('#^([ \t]+)([^ ])#', $line, $matches))
+			{
+				if ($min_length === null)
+				{
+					$min_length = \strlen($matches[1]);
+				}
+				else if (\strlen($matches[1]) < $min_length)
+				{
+					$min_length = \strlen($matches[1]);
+				}
+			}
+			else if ( ! $ignore_zero_indent)
+			{
+				$min_length = 0;
+				break;
+			}
+		}
+		
+		// unify
+		foreach ($lines as & $line)
+		{
+			if (\preg_match('#^[ \t].*#', $line))
+			{
+				$line = $indent.\substr($line, $min_length);
+			}
+			else # zero line
+			{
+				$line = $indent.$line;
+			}
+		}
+		
+		return \implode("\n", $lines);
+	}
 
 } # class
