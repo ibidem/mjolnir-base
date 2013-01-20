ca2a51850fe47b03b69ee0f485b5f25adb835620
srcspider <source.spider@gmail.com>
2013-01-21 00:55:37 +0200

    deprecating obsolete classes; some minor refactoring to seassion

--- Session/Native.php
+++ Session/Native.php
@@ -7,26 +7,27 @@
  * @copyright  (c) 2012, Ibidem Team
  * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
  */
-class Session_Native extends \app\Instantiatable
-	implements \mjolnir\types\Params
+class Session_Native extends \app\Instantiatable implements \mjolnir\types\Meta
 {
+	use \app\Trait_Meta;
+
 	/**
-	 * @var boolean 
+	 * @var boolean
 	 */
 	private $destroyed = false;
-	
+
 	/**
-	 * @return \mjolnir\base\Session_Native 
+	 * @return \mjolnir\base\Session_Native
 	 */
 	static function instance()
 	{
-		static $instance = null; 
-		
+		static $instance = null;
+
 		// session exists?
 		if ($instance === null)
 		{
 			$instance = parent::instance();
-			
+
 			// load session configuration
 			$session_config = \app\CFS::config('mjolnir/sessions');
 			$base_config = \app\CFS::config('mjolnir/base');
@@ -38,17 +39,17 @@ class Session_Native extends \app\Instantiatable
 					$session_config['secure'],   # secure
 					$session_config['httponly']  # httponly
 				);
-			
+
 			// start session
 			\session_start();
-			
+
 			// write the session at shutdown
 			\register_shutdown_function(array($instance, 'close'));
 		}
-		
+
 		return $instance;
 	}
-	
+
 	/**
 	 * @return boolean
 	 */
@@ -58,7 +59,7 @@ class Session_Native extends \app\Instantiatable
 		{
 			return false;
 		}
-		
+
 		try
 		{
 			\session_write_close();
@@ -70,9 +71,9 @@ class Session_Native extends \app\Instantiatable
 			return false;
 		}
 	}
-	
+
 	/**
-	 * @return boolean status 
+	 * @return boolean status
 	 */
 	function destroy()
 	{
@@ -80,17 +81,17 @@ class Session_Native extends \app\Instantiatable
 		{
 			return true;
 		}
-		
+
 		// destroy the current session
 		\session_destroy();
-		
+
 		$this->destroyed = true;
 
 		return true;
 	}
 
 	/**
-	 * @return int id 
+	 * @return int id
 	 */
 	function regenerate()
 	{
@@ -98,15 +99,15 @@ class Session_Native extends \app\Instantiatable
 		\session_regenerate_id();
 		return \session_id();
 	}
-	
+
 	/**
-	 * @return mixed parameter or default 
+	 * @return mixed parameter or default
 	 */
 	function get($key, $default = null)
 	{
 		return \array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
 	}
-	
+
 	/**
 	 * @param string key
 	 * @param mixed value
@@ -115,10 +116,10 @@ class Session_Native extends \app\Instantiatable
 	function set($key, $value)
 	{
 		$_SESSION[$key] = $value;
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * @param array associative array of key values
 	 * @return \mjolnir\types\Params $this
@@ -126,10 +127,10 @@ class Session_Native extends \app\Instantiatable
 	function populate_params(array $params)
 	{
 		$_SESSION = $params;
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * @return array
 	 */
