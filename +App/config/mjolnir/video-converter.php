30a91134ed3ed12acd00503b50f5fc446ed59cb6
srcspider <source.spider@gmail.com>
2013-03-08 11:26:54 +0200

    updated conversion defaults

--- +App/config/mjolnir/video-converter.php
+++ +App/config/mjolnir/video-converter.php
@@ -12,11 +12,14 @@
 					(
 						'mp4' => array
 							(
-								'flv' => '-ar 44100 -qscale:v 1'
+								'flv' => '-ar 44100 -qscale:v 1',
+								'ogv' => '-acodec libvorbis -vcodec libtheora -b 1000k',
 							),
 						'ogv' => array
 							(
-								'mp4' => '-b:v 1000k'
+								'mp4' => '-b:v 1000k',
+								'webm' => '-b:v 1000k',
+								'flv' => '-b:v 1000k',
 							)
 					)
 			)
